57f253532fde4f2e56f469a61884965d8593081c
Hugo Pereira <PereiraHugo@users.noreply.github.com>
2018-11-27 21:42:29 +1300

    Restructuration and Starting Unit Test

---
+++ Code/AnalyzingPings.UnitTest/PingStatisticsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyzingPings.UnitTest
+{
+    [TestClass]
+    public class PingStatisticsTests
+    {
+        [TestMethod]
+        public void ComputeMean_()
+        {
+            //Arrange
+            var pingstat = new PingStatistics();
+            //Act
+            var result = pingstat.ComputeMean()
+            //Assert
+            Assert.AreEqual()
+        }
+    }
+}
---
+++ Code/AnalyzingPings.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalyzingPings.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalyzingPings.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19fbb7e8-0dad-4099-8480-51cb28b69352")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
